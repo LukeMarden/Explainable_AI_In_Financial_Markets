b7175b854d04f289ebb8fd6a790758652ccda104
LukeMarden <55838911+LukeMarden@users.noreply.github.com>
2022-06-30 23:01:24 +0100

    feature construction - rsi

--- feature_construction.py
+++ feature_construction.py
@@ -15,9 +15,17 @@ def bollinger_bands(data, num_of_days, band_size=2):
     lower_band = pd.Series((sma - std * band_size), name='lower_bollinger_band')
     return upper_band, lower_band
 
-
-
-
+def relative_strength_index(data, period=14, simple=True):
+    delta = data['Close'].diff()
+    up = delta.clip(lower=0)
+    down = delta.clip(upper=0) * -1
+    if simple is True: #simple moving average
+        up_moving_average = simple_moving_average(up, period)
+        dowm_moving_average = simple_moving_average(down, period)
+    else: #exponential moving average
+        up_moving_average = exponential_moving_average(up, period)
+        dowm_moving_average = exponential_moving_average(down, period)
+    return 100 - (100/(1 + up_moving_average/dowm_moving_average)) #rsi
 
 
 
@@ -25,4 +33,4 @@ def bollinger_bands(data, num_of_days, band_size=2):
 if __name__ == '__main__':
     token = 'MSFT'
     data = pd.read_csv('data/s&p500/' + token + '.csv')
-    print(moving_average(data, 20))
+    print(simple_moving_average(data, 20))
